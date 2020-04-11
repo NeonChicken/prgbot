767eb5276d6c1d9df8a5f718beb1f0cd6916427a
NeonChicken <lcvandort@hotmail.com>
2020-04-11 22:01:26 +0200

    insultme - beledigme
    Insultme now has an English database.
    Beledigme contains the old Dutch one.

--- command_handlers/beledigme.py
+++ command_handlers/beledigme.py
@@ -1,7 +1,7 @@
 import random
 
 async def run(message):
-    with open('./resources/insults/insults-eng.txt') as insults:
-        with open('./resources/insults/adjectives-eng.txt') as adjectives:
+    with open('./resources/insults/insults-nl.txt') as insults:
+        with open('./resources/insults/adjectives-nl.txt') as adjectives:
             response = random.choice(list(adjectives)).capitalize().rstrip() + ' ' + random.choice(list(insults))
             await message.channel.send(response)
