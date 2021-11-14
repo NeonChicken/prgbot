0f28add6d25d225b1b0a497b608d8593b85ac832
Marco Breemhaar <marcobreemhaar@gmail.com>
2021-11-14 17:19:20 +0100

    Added insultme and beledigme commands

---
+++ command_handlers/insultme.py
@@ -0,0 +1,10 @@
+import random
+
+async def run(message):
+    response = generate_insult().capitalize()
+    await message.channel.send(response)
+
+def generate_insult():
+    with open('./resources/insults/insults-eng.txt') as insults:
+        with open('./resources/insults/adjectives-eng.txt') as adjectives:
+            return random.choice(list(adjectives)).rstrip() + ' ' + random.choice(list(insults)).rstrip()
